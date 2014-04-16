ebcabbee3262900c50e2398ef9c03be83409dab4
gthetwong <gthetwong@gmail.com>
2014-04-16 16:18:53 -0700

    same error, added document ready to append and graph in router

--- public/javascript/client.js
+++ public/javascript/client.js
@@ -318,8 +318,8 @@ loraxApp.Routers.Main = Backbone.Router.extend({
 
       console.log(a_model,"the model");
       var detailView = new loraxApp.Views.PlantDetailView({ model: a_model });
-      $('body').append(detailView.render().el);
-      detailView.graph();
+      $(document).ready( function(){$('body').append(detailView.render().el);
+      detailView.graph();});
       }
     });
 
