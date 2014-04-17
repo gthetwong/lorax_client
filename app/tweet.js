ddb72d66095bbd24f172529e300ae1ce70c79690
gthetwong <gthetwong@gmail.com>
2014-04-17 16:51:42 -0700

    trying to debug tweeting problem. can't get it to tweet, but no errors either. Seems like it's firing the post request to twitter, but its not successful because there's no tweet. No error log either

--- app/tweet.js
+++ app/tweet.js
@@ -40,10 +40,11 @@ exports.sendTweet = function(req,res){
           console.log(been_dry);
           console.log(user.username);
           console.log(plant.nickname);
+          console.log(access.at);
+          console.log(access.ats);
           switch(been_dry) {
             case 2:
-              tweet.statuses("update", {status: "@"+user.twitter.username+" please water me, sincerely "+plant.nickname+"#projectlorax "+timeNow}, 
-              access.at, access.ats, function(er, d, r){
+              tweet.statuses("update", {status: "@"+user.twitter.username+" please water me, sincerely "+plant.nickname+"#projectlorax "+timeNow}, access.at, access.ats, function(er, d, r){
                 if(er){
                   console.log(er);
                 }
